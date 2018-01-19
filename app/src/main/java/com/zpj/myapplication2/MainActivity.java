71dd53aac18ac2ad2c52ef23f259612b2c8c7dd2
zpjgit <zpj.love@foxmail.com>
2018-01-19 11:42:18 +0800

    1. Toast用法 2. Intent 启用另一个活动

--- app/src/main/java/com/zpj/myapplication2/MainActivity.java
+++ app/src/main/java/com/zpj/myapplication2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.zpj.myapplication2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(com.zpj.myapplication2.MainActivity.this, "启动第二个活动",
+                    Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
